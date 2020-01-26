547372553453db4d9f7c525e36a5c47d4063f6e1
Yana Maximkina <djanglboop@gmail.com>
2020-01-26 18:27:15 +0900

    Add some styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,18 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+    ActionMailer::Base.smtp_settings = {
+        :address => 'smtp.sendgrid.net',
+        :port => '587',
+        :authentication => :plain,
+        :user_name => Rails.application.credentials[:sendgrid][:user_name],
+        :password => Rails.application.credentials[:sendgrid][:password],
+        :domain => 'heroku.com',
+        :enable_starttls_auto => true
+      }
+      config.action_mailer.delivery_method = :test
+      config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -60,3 +72,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,18 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+    config.action_mailer.smtp_settings = {
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :domain => 'heroku.com',
+        :address => 'smtp.sendgrid.net',
+        :port => 587,
+        :authentication => :plain,
+        :enable_starttls_auto => true
+      }
+      config.action_mailer.delivery_method = :smtp
+      config.action_mailer.default_url_options = { :host => 'yanamax-photo-app.herokuapp.com/', :protocol => 'https' }
+  
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
